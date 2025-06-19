df9ecb2ca5e5a597c9eef29b711ee88474bc77db
Chad Dombrova <chadrik@gmail.com>
2025-06-19 10:49:20 -0700

    Address a few changes after the switch to uv

--- usd/stubgen_usd.sh
+++ usd/stubgen_usd.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+REPO_PATH=$(git rev-parse --show-toplevel)
 outdir=$REPO_PATH/usd/stubs
 
 # Custom variables --
@@ -14,13 +15,10 @@ export USD_XML_INDEX="${USD_BUILD_ROOT}/docs/doxy_xml/index.xml"
 export PYTHONPATH=$USD_BUILD_ROOT/lib/python:$USD_SOURCE_ROOT/docs/python
 export PXR_USD_PYTHON_DISABLE_DOCS=1
 
-# clean first, in case modules have been removed (or stubgen is silently failing)
-find $outdir/pxr -name '*.pyi' -type f -delete
-
 # USD is a mixture of pure python (e.g. pxr.Sdf.__init__) and extension modules (pxr.Sdf._sdf), and
 # the __init__ modules do runtime injection, so parsing these modules produces bad results..
-export UV_PYTHON=$(which python3)
-uv run ./stubgen_usd.py
+#export UV_PYTHON=$(which python3)
+uv run ./stubgen_usd.py $outdir
 
 rm -f $outdir/pxr/*/_[a-z]*.pyi
 rm -f $outdir/pxr/*/__DOC.pyi
