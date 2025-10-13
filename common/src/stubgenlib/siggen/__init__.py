d42306daf45b9768dafbae0e565917b3b664781e
Chad Dombrova <chadrik@gmail.com>
2025-10-13 10:39:08 -0700

    mypy: don't run mypy against the stubs
    mypy-silent is used to silence errors in the stubs

--- common/src/stubgenlib/siggen/__init__.py
+++ common/src/stubgenlib/siggen/__init__.py
@@ -1,7 +1,20 @@
 from __future__ import absolute_import, print_function
 
-from .advanced import AdvancedSigMatcher, AdvancedSignatureGenerator, Optionality
-from .boost import BoostDocstringSignatureGenerator
-from .default import DefaultSigGenerator
-from .docstring import DocstringSignatureGenerator
-from .sigfixer import DocstringTypeFixer, SignatureFixer
+from .advanced import (
+    AdvancedSigMatcher as AdvancedSigMatcher,
+)
+from .advanced import (
+    AdvancedSignatureGenerator as AdvancedSignatureGenerator,
+)
+from .advanced import (
+    Optionality as Optionality,
+)
+from .boost import BoostDocstringSignatureGenerator as BoostDocstringSignatureGenerator
+from .default import DefaultSigGenerator as DefaultSigGenerator
+from .docstring import DocstringSignatureGenerator as DocstringSignatureGenerator
+from .sigfixer import (
+    DocstringTypeFixer as DocstringTypeFixer,
+)
+from .sigfixer import (
+    SignatureFixer as SignatureFixer,
+)
